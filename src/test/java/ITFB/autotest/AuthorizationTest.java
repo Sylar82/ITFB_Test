d860e5a41632c0cd78e5761578e0a13f8d099864
Admin <stason2895@gmail.com>
2018-08-14 14:28:54 +0300

    добавлен тест на авторизацию пользователя

---
+++ src/test/java/ITFB/autotest/AuthorizationTest.java
@@ -0,0 +1,50 @@
+package ITFB.autotest;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class AuthorizationTest {
+
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup(){
+
+        System.setProperty("webdriver.chrome.driver", "/Volumes/HDD/Development/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://newtours.demoaut.com/");
+
+
+    }
+
+    @Test
+    public void UserLogin(){
+        WebElement loginField = driver.findElement(By.xpath("/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input"));
+        loginField.sendKeys("test1");
+
+        WebElement passwordField = driver.findElement(By.xpath("/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[3]/td[2]/input"));
+        passwordField.sendKeys("test1");
+
+        WebElement loginButton = driver.findElement(By.xpath("/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[4]/td[2]/div/input"));
+        loginButton.click();
+
+        driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/img")).isDisplayed();
+        Assert.assertTrue(true);
+
+        driver.quit();
+
+
+    }
+
+
+
+}

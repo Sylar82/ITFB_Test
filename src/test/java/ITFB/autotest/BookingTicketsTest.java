3917d7799636460720ecbb2f7db6ec31a3199739
Admin <stason2895@gmail.com>
2018-08-14 14:33:09 +0300

    добавлен тест на авторизацию пользователя

--- src/test/java/ITFB/autotest/BookingTicketsTest.java
+++ src/test/java/ITFB/autotest/BookingTicketsTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class AuthorizationTest {
+public class BookingTicketsTest {
 
     private static WebDriver driver;
 
